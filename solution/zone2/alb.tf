8d530cdba3865daf7bba5cdbd849bb1f9537961b
Yoshiya Magana Martinez (FG-420) <yoshiya.magana-martinez@bmw.de>
2024-12-09 05:17:17 +0100

    fix broken source path after reorganising

--- solution/zone2/alb.tf
+++ solution/zone2/alb.tf
@@ -1,5 +1,5 @@
 module "project_alb" {
-   source             = "modules/alb"
+   source             = "./modules/alb"
    ec2                = module.project_ec2.aws_instance
    ec2_sg             = module.project_ec2.ec2_sg
    subnet_id          = module.vpc.public_subnet_ids
